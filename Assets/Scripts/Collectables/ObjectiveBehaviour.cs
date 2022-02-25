bbdf8517b245b9ccac3da26812a9b7f199576dd2
Sean Walton <sean@pillbug.zone>
2022-02-25 12:47:25 +0000

    Added an objective prefab

---
+++ Assets/Scripts/Collectables/ObjectiveBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveBehaviour : MonoBehaviour
+{
+    public void OnCollect()
+    {
+        Debug.Log("Collected Objective");
+    }
+}
