9339ab6d9ed5ea5a9f5eb7a73fbadb276a198b5c
Sean Walton <sean@pillbug.zone>
2022-03-18 13:54:19 +0000

    Added explosion effect to player death

--- Assets/Scripts/Player Scripts/PlayerManager.cs
+++ Assets/Scripts/Player Scripts/PlayerManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerManager : MonoBehaviour
 {
+    public UnityEvent OnKill;
     public void Kill()
+    {
+        OnKill?.Invoke();
+        Invoke("DeactiveObject", 0.5f);
+    }
+
+    private void DeactiveObject()
     {
         gameObject.SetActive(false);
     }
