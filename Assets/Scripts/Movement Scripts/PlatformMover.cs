4ed250fd41ea770256b5060c9b1f9b22b454dbf9
Sean Walton <sean@pillbug.zone>
2022-03-18 13:25:46 +0000

    The platform movers now calculate velocity

--- Assets/Scripts/Movement Scripts/PlatformMover.cs
+++ Assets/Scripts/Movement Scripts/PlatformMover.cs
@@ -9,25 +9,29 @@ public class PlatformMover : MonoBehaviour
     [SerializeField] private float yMoveDistance;
     [SerializeField] private float xMoveDistance;
     [SerializeField] private float timeToMove;
+
     private Transform tr;
     private Sequence ySequence;
     private Sequence xSequence;
+
+    public Vector2 velocity { private set; get; }
+    private Vector2 lastPosition = new Vector2();
 
     public bool movingX { private set; get; }
     public bool movingY { private set; get; }
 
     private Vector3 startPos;
+    private Vector2 newVelocity = new Vector2();
 
     private void Awake()
     {
         tr = transform;
         movingX = false;
         movingY = false;
-
+        velocity = new Vector2(0f, 0f);
         startPos = tr.position;
+        lastPosition = tr.position;
     }
-
-
 
     public void StartXMotion()
     {
@@ -67,5 +71,19 @@ public class PlatformMover : MonoBehaviour
         ySequence.SetLoops(-1, LoopType.Yoyo);
     }
 
-    
+    private void Update()
+    {
+        CalculateVelocity(Time.deltaTime);
+    }
+
+
+    private void CalculateVelocity(float dt)
+    {
+        newVelocity.x = (tr.position.x - lastPosition.x) / dt;
+        newVelocity.y = (tr.position.y - lastPosition.y) / dt;
+
+        velocity = newVelocity;
+
+        lastPosition = tr.position;
+    }
 }
